4415a01da3c13966716de9e93f8cf00ce1edfed7
Josh Seides <me@joshseides.com>
2017-04-11 15:50:16 -0400

    update

--- index.js
+++ index.js
@@ -1,50 +1,33 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send('The John Bot (server)');
-});
-
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'johnbot_verify_token') {
-        res.send(req.query['hub.challenge']);
+// Webhook validation
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
     } else {
-        res.send('Invalid verify token');
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
     }
 });
 
-/*// handler receiving messages
-app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            var args = event.message.text.split(' '), keyword = args[0];
-            if (keyword === 'kitten' && args.length === 3) {
-                kittenMessage(event.sender.id, event.message.text);
-            } else if (keyword === 'spaghetti') {
-                navySealMessage(event.sender.id, event.message.text);
-            } else if (keyword === 'test') {
-                sendMessage(event.sender.id, {text: "Hello."});
-            } else {
-                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-            }
-        } else if (event.postback) {
-            console.log("Postback received: " + JSON.stringify(event.postback));
-        }
-    }
-    res.sendStatus(200);
-});*/
+// Display the web page
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
+// Message processing
 app.post('/webhook', function (req, res) {
+    console.log(req.body);
     var data = req.body;
 
     // Make sure this is a page subscription
@@ -59,6 +42,8 @@ app.post('/webhook', function (req, res) {
             entry.messaging.forEach(function(event) {
                 if (event.message) {
                     receivedMessage(event);
+                } else if (event.postback) {
+                    receivedPostback(event);
                 } else {
                     console.log("Webhook received unknown event: ", event);
                 }
@@ -66,10 +51,15 @@ app.post('/webhook', function (req, res) {
         });
 
         // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
         res.sendStatus(200);
     }
 });
 
+// Incoming events handling
 function receivedMessage(event) {
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
@@ -86,9 +76,8 @@ function receivedMessage(event) {
     var messageAttachments = message.attachments;
 
     if (messageText) {
-
         // If we receive a text message, check to see if it matches a keyword
-        // and send back the example. Otherwise, just echo the text we received.
+        // and send back the template example. Otherwise, just echo the text we received.
         switch (messageText) {
             case 'generic':
                 sendGenericMessage(senderID);
@@ -102,10 +91,26 @@ function receivedMessage(event) {
     }
 }
 
-function sendGenericMessage(recipientId, messageText) {
-    // To be expanded in later sections
+function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback
+    // button for Structured Messages.
+    var payload = event.postback.payload;
+
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
 }
 
+//////////////////////////
+// Sending helpers
+//////////////////////////
 function sendTextMessage(recipientId, messageText) {
     var messageData = {
         recipient: {
@@ -119,10 +124,57 @@ function sendTextMessage(recipientId, messageText) {
     callSendAPI(messageData);
 }
 
+function sendGenericMessage(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [{
+                        title: "rift",
+                        subtitle: "Next-generation virtual reality",
+                        item_url: "https://www.oculus.com/en-us/rift/",
+                        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                        buttons: [{
+                            type: "web_url",
+                            url: "https://www.oculus.com/en-us/rift/",
+                            title: "Open Web URL"
+                        }, {
+                            type: "postback",
+                            title: "Call Postback",
+                            payload: "Payload for first bubble",
+                        }],
+                    }, {
+                        title: "touch",
+                        subtitle: "Your Hands, Now in VR",
+                        item_url: "https://www.oculus.com/en-us/touch/",
+                        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                        buttons: [{
+                            type: "web_url",
+                            url: "https://www.oculus.com/en-us/touch/",
+                            title: "Open Web URL"
+                        }, {
+                            type: "postback",
+                            title: "Call Postback",
+                            payload: "Payload for second bubble",
+                        }]
+                    }]
+                }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
 function callSendAPI(messageData) {
     request({
         uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: PAGE_ACCESS_TOKEN },
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
         method: 'POST',
         json: messageData
 
@@ -141,76 +193,7 @@ function callSendAPI(messageData) {
     });
 }
 
-/*function chunkText(text) {
-    var chunks = text.match(/(.|[\r\n]){1,600}/g);
-    return chunks;
-};
-
-// generic function sending messages
-function sendMessage(recipientId, message) {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: PAGE_ACCESS_TOKEN },
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
-
-function navySealMessage(recipientId, text) {
-    if (text.toLowerCase() === 'spaghetti') {
-        var navySealCopypasta = "hi";
-        var chunkySeal = chunkText(navySealCopypasta);
-        chunkySeal.reverse().forEach(function(chunk) {
-            //TODO: FIX THIS QUICK FIX
-            setTimeout(function() {
-                sendMessage(recipientId, {text: chunk});
-            }, 100);
-        });
-        return true;
-    }
-    return false;
-}
-
-// send rich message with kitten
-function kittenMessage(recipientId, text) {
-    var values = text.split(' ');
-    if (Number(values[1]) > 0 && Number(values[2]) > 0) {
-
-        var imageUrl = "https://placekitten.com/" + Number(values[1]) + "/" + Number(values[2]);
-
-        message = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Kitten",
-                        "subtitle": "Cute kitten picture",
-                        "image_url": imageUrl ,
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": imageUrl,
-                            "title": "Show kitten"
-                        }, {
-                            "type": "postback",
-                            "title": "I like this",
-                            "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                        }]
-                    }]
-                }
-            }
-        };
-        sendMessage(recipientId, message);
-        return true;
-    }
-    return false;
-};*/
+// Set Express to listen out for HTTP requests
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log("Listening on port %s", server.address().port);
+});
